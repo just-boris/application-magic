8ea145e21a6de85fb70758025dddd2e5c7420261
Boris <just-boris@hotmail.com>
2013-02-16 01:49:24 +0400

    * немного текста в первую главу * немного во вторую

--- make.bat
+++ make.bat
@@ -1,5 +1,5 @@
 SET PATH=c:\Program Files (x86)\Inkscape;%PATH%
 for /f "tokens=1,* delims=." %%i in ('dir /b img_static *.svg') do (
-  inkscape -f img_static\%%i.%%j -A img\%%i.pdf
+  inkscape -z -f img_static\%%i.%%j -A img\%%i.pdf
 )
 pdflatex -synctex=1 --output-directory=build -interaction=nonstopmode main.tex
