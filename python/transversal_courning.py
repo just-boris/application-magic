9ffea937ea00c1385ad0bbca4a6ebc6114d75542
Boris <just-boris@hotmail.com>
2013-04-30 01:30:28 +0400

    изменена часть про поперечное смещение добавлена картинка переменного радиуса

---
+++ python/transversal_courning.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+import pylab
+import numpy as np
+import main
+from wg_base.coupling import coupling
+from gauss import Gauss
+from planar import Planar
+
+outputW = Planar(open('matrix/dump2d.csv'))
+r = main.wg_radius['Corning']
+
+def substrat(x):
+    inputW = Gauss(r, r, 0, x)
+    result = coupling(inputW.func, outputW.func)
+    if result > substrat.maxVal:
+        substrat.maxVal = result
+        substrat.maxArg = r
+    return result
+substrat.maxVal = 0
+
+xmin = main.xmin
+xmax = main.xmax
+maxVal = 0
+
+x = np.arange(xmin, xmax, 0.1)
+pylab.plot(x, map(substrat, x))
+pylab.annotate("Cmax = {0:.3f}".format(substrat.maxVal), (5, 0.75))
+main.arrow_axes((xmin, xmax), (0, 1), "$\Delta x$", "C")
+
+pylab.savefig(main.getOutImagePath(__file__))
---
+++ python/var_radius.py
@@ -0,0 +1,34 @@
+# coding=utf-8
+import math
+import pylab
+import numpy as np
+import main
+from wg_base.coupling import coupling
+from scipy.optimize import fmin_powell
+from gauss import Gauss
+from planar import Planar
+
+planar = Planar(open('matrix/dump2d.csv'))
+
+def intersect(r, x):
+    cylinder = Gauss(r, r, x[0], x[1])
+    return coupling(planar.func, cylinder.func)
+
+def var_radius(r):
+    result = intersect(r, fmin_powell(lambda x: -intersect(r, x), [0, -2]))
+    if result > var_radius.maxVal:
+        var_radius.maxVal = result
+        var_radius.maxArg = r
+    return result
+var_radius.maxVal = 0
+
+xmin = 1
+xmax = 5
+
+x = np.arange(xmin, xmax+1, 0.2)
+pylab.plot(x, map(var_radius, x))
+pylab.annotate("Cmax = {0:.3f}".format(var_radius.maxVal), (3, 1))
+pylab.annotate("r_max = {0:.1f}".format(var_radius.maxArg), (3, 0.9))
+main.arrow_axes((xmin, xmax), (0, 1.2), "$r$", "C")
+
+pylab.savefig(main.getOutImagePath(__file__))
