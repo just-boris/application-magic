7360db5fe29415af21d0ac7444f55f4a17b805f2
Boris <just-boris@hotmail.com>
2013-04-30 22:25:42 +0400

    добавил подписи с обозначением волноводов описал в тексте свои задачи

--- python/var_radius.py
+++ python/var_radius.py
@@ -10,6 +10,12 @@ from planar import Planar
 
 planar = Planar(open('matrix/dump2d.csv'))
 
+def annotateWaveguide(name):
+    x = main.wg_radius[name]
+    y = var_radius(main.wg_radius[name])
+    pylab.annotate(name, xy=(x, y),  xycoords='data', xytext=(40, 50), textcoords='offset points',
+                   arrowprops=dict(arrowstyle="->", connectionstyle="arc,angleA=180,armA=50,rad=8"))
+
 def intersect(r, x):
     cylinder = Gauss(r, r, x[0], x[1])
     return coupling(planar.func, cylinder.func)
@@ -23,12 +29,15 @@ def var_radius(r):
 var_radius.maxVal = 0
 
 xmin = 1
-xmax = 5
+xmax = 7
 
 x = np.arange(xmin, xmax+1, 0.2)
 pylab.plot(x, map(var_radius, x))
+annotateWaveguide('Corning')
+annotateWaveguide('FOG')
+annotateWaveguide('PANDA')
 pylab.annotate("Cmax = {0:.3f}".format(var_radius.maxVal), (3, 1))
-pylab.annotate("r_max = {0:.1f}".format(var_radius.maxArg), (3, 0.9))
+pylab.annotate("Rmax = {0:.1f}".format(var_radius.maxArg), (3, 0.9))
 main.arrow_axes((xmin, xmax), (0, 1.2), "$r$", "C")
 
 pylab.savefig(main.getOutImagePath(__file__))
