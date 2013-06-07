ce7329a1e6ebe278e553f6915e58ef56dacd1ea2
Boris <just-boris@hotmail.com>
2013-06-07 23:03:25 +0400

    добавил readme

--- python/wg_plot/plot3d.py
+++ python/wg_plot/plot3d.py
@@ -5,7 +5,7 @@ from mpl_toolkits.mplot3d import Axes3D
 
 def plot(x, y, f):
     xgrid, ygrid = np.meshgrid(x, y)
-    zgrid = map(lambda x, y: map(f, x, y), xgrid, ygrid)
+    zgrid = [map(f, x, y) for (x, y) in zip(xgrid, ygrid)]
     fig = pylab.figure()
     #TODO ax = fig.gca(projection='3d')
     Axes3D(fig).plot_surface(xgrid, ygrid, zgrid, rstride=1, cstride=1, color="#999999")
